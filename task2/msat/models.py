18c5337e1ef55ed39c9bb6d09768891ab93f9d88
ash264 <ashishsharma8445@gmail.com>
2020-05-18 14:59:55 +0530

    task 2(MSAT)

---
+++ task2/msat/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class MSAT(models.Model):
+    r_id = models.IntegerField()
+    stud_id = models.CharField(max_length = 50)
+    response = models.CharField(max_length = 500)
+    timestamp = models.IntegerField()  # To store duartion in seconds
+    mark_scored = models.IntegerField()
+    answered = models.CharField(max_length = 500)
+
+    def __str__(self):
+    	return self.stud_id
